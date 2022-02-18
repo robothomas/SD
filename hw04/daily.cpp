c19fd75395a806a710399043e4da8e4dc930c8e3
Dylan Thomas <thomas31@stolaf.edu>
2022-02-18 15:13:12 -0600

    homework 4 daily (from yesterday) - planning to work on this more on homework 5

---
+++ hw04/daily.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void askForInput() {
+    char typeOfInput = 'N';
+    cout << "Type U for a list of users, A to add a user, or T to add a transaction" << endl;
+    if (typeOfInput == 'U') {
+
+    }
+}
+
+void list() {
+
+}
+
+int main() {
+    cout << "Welcome to an example bank registry." << endl; 
+    return 0;
+}
